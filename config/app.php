aa66efb382a4603fc375d088bc0ca4ea63c17f94
Bogdan Kuts <bogdan.kootz@bzzz.biz.ua>
2016-07-15 15:33:47 +0300

    wtf commit

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
+        
 
     ],
 
